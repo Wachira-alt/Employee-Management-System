826dc66c40ce12362e45ca4ccb0d429fc42844b0
Dennis Wachira <wachiradenis336@gmail.com>
2025-05-23 23:18:26 +0300

    feat(schema): add employees table with foreign key to departments

---
+++ db/schema/002_create_employees.sql
@@ -0,0 +1,18 @@
+CREATE TABLE employees(
+  id INTEGER PRIMARY KEY,
+  full_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  hire_date TEXT NOT NULL,
+  role TEXT NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) references departments(id)
+   ON DELETE SET NULL  
+   ON UPDATE CASCADE 
+
+);
+
+--ON DELETE SET NULL -> if a department is deleted, don't delete the employee, just remove the link
+
+--ON UPDATE CASCADE -> if dep ID changes, update here too
+
+--applying it --> $ sqlite3 db/library.db < db/schema/002_create_employees.sql
