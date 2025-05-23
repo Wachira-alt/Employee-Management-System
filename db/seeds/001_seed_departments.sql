5f693bc385de42a08136e9213480f0b4de8adbd4
Dennis Wachira <wachiradenis336@gmail.com>
2025-05-23 23:33:41 +0300

    chore(seed): add initial departments data

---
+++ db/seeds/001_seed_departments.sql
@@ -0,0 +1,9 @@
+INSERT INTO departments (name, location)
+VALUES
+  ('Engineering', 'Nairobi'),
+  ('IT Support', 'Mombasa'),
+  ('Human Resources', 'Nakuru');
+
+
+-- to see the changes --> $ sqlite3 db/library.db < db/seeds/001_seed_departments.sql
+
