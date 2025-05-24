ca014d0b0014cb1e14043411973fa1859a71654a
Dennis Wachira <wachiradenis336@gmail.com>
2025-05-24 11:38:24 +0300

    feat(schema): create join table employee_projects to map many-to-many employee-project relationship

---
+++ db/schema/004_create_employee_projects_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS employee_projects (
+  employee_id INTEGER,
+  project_id INTEGER,
+  FOREIGN KEY (employee_id) REFERENCES employees(id),
+  FOREIGN KEY (project_id) REFERENCES projects(id)
+);
