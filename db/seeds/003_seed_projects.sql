251c0f417e3373d4f35262ae9b64949d333eccc5
Dennis Wachira <wachiradenis336@gmail.com>
2025-05-24 11:43:53 +0300

    seed: add initial data to projects and employee_projects join table

---
+++ db/seeds/003_seed_projects.sql
@@ -0,0 +1,3 @@
+INSERT INTO projects (name, deadline) VALUES
+  ("Website Redesign", "2025-06-30"),
+  ("Security Upgrade", "2025-07-15");
---
+++ db/seeds/004_seed_employee_projects.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee_projects (employee_id, project_id) VALUES
+  (1, 1),  -- Employee 1 is on Website Redesign
+  (1, 2),  -- Employee 1 is also on Security Upgrade
+  (2, 1);  -- Employee 2 is on Website Redesign
